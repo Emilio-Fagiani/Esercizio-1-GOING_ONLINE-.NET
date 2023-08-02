97537346694cc3f01f8cd30929981c4e698c8088
emifa <emifag00@gmail.com>
2023-08-02 12:39:56 +0200

    Add code

---
+++ Esercizio-1-GOING_ONLINE-.NET/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+
+var httpClient  = new HttpClient();
+Console.WriteLine("Please insert the URL: ");
+var inpUrl = Console.ReadLine();
+var retriver = new InpUrl();
+Console.WriteLine(await retriver.RetriveUrl(inpUrl));
+
+public class InpUrl
+{
+    public async Task<string> RetriveUrl(string url)
+    {
+        var Http = new HttpClient();
+        var response = await Http.GetAsync(url);
+        Console.WriteLine("----HEADERS----");
+        Console.WriteLine(response.Headers);
+        Console.WriteLine("----CONTENT----");
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+        return string.Empty;
+    }
+}
+
